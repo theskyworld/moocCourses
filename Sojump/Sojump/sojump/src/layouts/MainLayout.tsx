90e53dbdbe477d1cb76e3042fde3fa77704dcf9d
theskyworld <2456450687@qq.com>
2023-08-28 17:29:45 +0800

    * refactor : refactor MainLayout.tsx with antd

--- Sojump/Sojump/sojump/src/layouts/MainLayout.tsx
+++ Sojump/Sojump/sojump/src/layouts/MainLayout.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import styles from "./MainLayout.module.scss";
+
+const { Header, Content, Footer } = Layout;
 
 const MainLayout: FC = () => {
     return (
-        <>
-            <div>MainLayout header</div>
-            <div>
+        <Layout>
+            <Header className={styles.header}>
+                <div className={styles.left}>logo</div>
+                <div className={styles.right}>登录</div>
+            </Header>
+            <Content className={styles.main}>
                 {/* 类似于vue中的slot,RouterView */}
                 <Outlet></Outlet>
-            </div>
-            <div>MainLayout footer</div>
-        </>
+            </Content>
+            <Footer className={styles.footer}>V问卷 &copy;2023 - now. Created by tsw</Footer>
+        </Layout>
+
     )
 }
 
