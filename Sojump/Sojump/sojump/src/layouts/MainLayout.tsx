6d914f1a4d575c0397a30b27d1ca0028adef34f8
theskyworld <2456450687@qq.com>
2023-08-28 15:02:26 +0800

    * feat : add layout

---
+++ Sojump/Sojump/sojump/src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { Outlet } from "react-router-dom";
+
+const MainLayout: FC = () => {
+    return (
+        <>
+            <div>MainLayout header</div>
+            <div>
+                {/* 类似于vue中的slot */}
+                <Outlet></Outlet>
+            </div>
+            <div>MainLayout footer</div>
+        </>
+    )
+}
+
+export default MainLayout;
