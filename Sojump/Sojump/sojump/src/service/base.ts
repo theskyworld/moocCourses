cf5c9fce0bc3a424581a930093218ea0c49463f8
theskyworld <2456450687@qq.com>
2023-08-30 09:07:40 +0800

    * feat : add response interceptor for axios

---
+++ Sojump/Sojump/sojump/src/service/base.ts
@@ -0,0 +1,36 @@
+import { message } from "antd";
+import axios from "axios";
+
+export interface Response {
+    errno: number;
+    data?: ResponseData;
+    msg?: string;
+}
+interface ResponseData {
+    [key: string]: any;
+}
+
+
+
+
+const instance = axios.create({
+    timeout : 10 * 1000,
+})
+
+// response拦截器
+instance.interceptors.response.use((res) => {
+    const response: Response = res.data || {};
+    const { errno, data, msg } = response;
+
+    if (errno !== 0) {
+        // 存在错误，进行错误提示
+        if (msg) {
+            message.error(msg);
+        }
+        throw new Error(msg);
+    }
+
+    return data as any
+})
+
+export default instance;
