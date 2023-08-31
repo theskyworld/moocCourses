da5723329c190ea278d6ef0e415b38034ac01566
theskyworld <2456450687@qq.com>
2023-08-31 11:24:08 +0800

    * feat : add UserInfo.tsx

--- Sojump/Sojump/sojump/src/components/UserInfo.tsx
+++ Sojump/Sojump/sojump/src/components/UserInfo.tsx
@@ -1,13 +1,54 @@
-import React, { FC } from "react";
-import { Link } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
+import { useRequest } from "ahooks";
+import { Button } from "antd";
+import React, { FC, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { LOGIN_URL } from "../assets/ts/constants";
+import { removeToken } from "../assets/utils/userToken";
+import { getUserInfoService } from "../service/user";
 
 const UserInfo: FC = () => {
-    
+    const [isLogined, setIsLogined] = useState(false);
+    const { data } = useRequest(getUserInfoService, {
+        onSuccess() {
+            setIsLogined(true);
+        }
+    });
+    const { username } = data || {};
+    const nav = useNavigate();
+
+    // 退出登录
+    function logout() {
+        // 清除本地存储的token
+        removeToken();
+        nav(LOGIN_URL)
+    }
+
+    // 展示用户信息的组件
+    const ShowUserInfo: FC = () => {
+        return (
+            <>
+                <span style={{ color: "#e8e8e8" }}>
+                    <UserOutlined />
+                    {username}
+                </span>
+                <Button type="link" onClick={logout}>退出登录</Button>
+            </>
+        )
+    }
+    // 展示登录的组件
+    const ShowLogin: FC = () => {
+        return (
+            <>
+                <Link to={LOGIN_URL}>登录</Link>
+            </>
+        )
+    }
+
     return (
-        <>
-            <Link to={LOGIN_URL}>登录</Link>
-        </>
+        <div>
+            {isLogined ? <ShowUserInfo /> : <ShowLogin />}
+        </div>
     )
 }
 
