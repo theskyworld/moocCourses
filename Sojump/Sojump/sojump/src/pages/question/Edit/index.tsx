fb8ed55242d09e12e315719e80b2cb45e66a7780
theskyworld <2456450687@qq.com>
2023-09-01 14:23:56 +0800

    * feat : add layout style for Edit page

--- Sojump/Sojump/sojump/src/pages/question/Edit/index.tsx
+++ Sojump/Sojump/sojump/src/pages/question/Edit/index.tsx
@@ -1,13 +1,28 @@
 import React, { FC } from "react";
 import useLoadQuestionData from "../../../hooks/useLoadQuestionData";
+import styles from "./IndexLayout.module.scss";
+
+
 const EditIndex: FC = () => {
 
     const { loading, data } = useLoadQuestionData();
 
     return (
         <>
-            <h3>EditPage</h3>
-            <p>{ loading ? "Loading..." : JSON.stringify(data)}</p>
+            <div className={styles.container}>
+                <div style={{ backgroundColor: "#fff" }}>Header</div>
+                <div className={styles['content-wrapper']}>
+                    <div className={styles.content}>
+                        <div className={styles.left}>Left</div>
+                        <div className={styles.main}>
+                            <div className={styles["canvas-wrapper"]}>
+                                <div style={{ height: "900px" }}>画布</div>
+                            </div>
+                        </div>
+                        <div className={styles.right}>Right</div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
