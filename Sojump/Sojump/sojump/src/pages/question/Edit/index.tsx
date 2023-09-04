4bd03c29484dc537a90dd61f04fe2fa3469785e9
theskyworld <2456450687@qq.com>
2023-09-04 20:00:00 +0800

    * feat : add Stat

--- Sojump/Sojump/sojump/src/pages/question/Edit/index.tsx
+++ Sojump/Sojump/sojump/src/pages/question/Edit/index.tsx
@@ -7,11 +7,16 @@ import { changeSelectedId } from "../../../store/componentsReducer";
 import LeftPanel from "../../../components/questionComponents/LeftPanel";
 import RightPanel from "../../../components/questionComponents/RightPanel";
 import EditHeader from "../../../components/questionComponents/EditHeader";
+import useGetPageSetting from "../../../hooks/useGetPageSetting";
+import { useTitle } from "ahooks";
 
 const EditIndex: FC = () => {
 
     const { loading} = useLoadQuestionDataWithComponents();
     const dispatch = useDispatch();
+    // 修改页面标题
+    const { title } = useGetPageSetting();
+    useTitle(`问卷编辑-${title}`);
 
     // 点击空白处时取消选中组件
     function clearSelectedId() {
--- Sojump/Sojump/sojump/src/pages/question/Stat/index.tsx
+++ Sojump/Sojump/sojump/src/pages/question/Stat/index.tsx
@@ -1,8 +1,58 @@
+import { useTitle } from "ahooks";
+import { Button, Result, Spin } from "antd";
+import { divide } from "lodash";
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import useGetPageSetting from "../../../hooks/useGetPageSetting";
+import useLoadQuestionDataWithComponents from "../../../hooks/useLoadQuestionDataWithComponents";
+import styles from "./StatLayout.module.scss";
+
 
 const StatIndex: FC = () => {
+    // 增加loading效果
+    const { loading } = useLoadQuestionDataWithComponents();
+    const {title, isPublished } = useGetPageSetting();
+    const nav = useNavigate();
+
+    // 修改页面标题
+    useTitle(`问卷统计-${title}`);
+
+
+    // loading
+    const LoadingElem = (
+        <div style={{ textAlign: "center", marginTop: "60px" }}><Spin tip="加载中..."><span></span></Spin></div>
+    );
+
+    function generateContentElem() {
+        // 如果该问卷尚未发布
+        if (typeof isPublished === 'boolean' && !isPublished) {
+            return (
+                <div style={{ flex: '1' }}>
+                    <Result
+                        status="warning"
+                        title="该页面尚未发布"
+                        extra={
+                            <Button type="primary" onClick={() => nav(-1)}>
+                                返回
+                            </Button>
+                        }
+                    ></Result>
+                </div>
+            )
+        }
+
+        return (
+            <p>统计页面</p>
+        )
+    }
     return (
-        <div>StatIndex</div>
+        <div className={styles.container}>
+            <p>头部</p>
+            <div className={styles['content-wrapper']}>
+                {loading && LoadingElem}
+                {!loading && <div className={styles.content}>{generateContentElem()}</div>}
+            </div>
+        </div>
     )
 }
 
