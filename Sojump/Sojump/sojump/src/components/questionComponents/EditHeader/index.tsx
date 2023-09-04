709e7e87bf8c342c45436fe53268697e8e004316
theskyworld <2456450687@qq.com>
2023-09-04 10:31:28 +0800

    * feat : add SaveButtonCom in EditHeader

--- Sojump/Sojump/sojump/src/components/questionComponents/EditHeader/index.tsx
+++ Sojump/Sojump/sojump/src/components/questionComponents/EditHeader/index.tsx
@@ -1,11 +1,14 @@
 // 编辑页面中的头部组件
 
-import { EditOutlined, LeftOutlined } from "@ant-design/icons";
+import { EditOutlined, LeftOutlined, LoadingOutlined } from "@ant-design/icons";
+import { useDebounce, useDebounceEffect, useKeyPress, useRequest } from "ahooks";
 import { Button, Input, Space, Typography } from "antd";
 import { ChangeEvent, FC, MouseEvent, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
 import useGetPageSetting from "../../../hooks/useGetPageSetting";
+import { updateQuestionService } from "../../../service/question";
 import { changePageTitle } from "../../../store/PageSettingReducer";
 import styles from "./EditHeaderLayout.module.scss";
 import EditToolbar from "./EditToolbar";
@@ -15,7 +18,7 @@ const EditHeader: FC = () => {
     const nav = useNavigate();
     const { Title } = Typography;
 
-    // 显示和修改标题
+    // 显示和修改标题组件
     const TitleCom: FC = () => {
         const { title } = useGetPageSetting()
         const dispatch = useDispatch();
@@ -47,6 +50,47 @@ const EditHeader: FC = () => {
         )
     }
 
+    // 保存按钮组件
+    // 保存按钮
+    const SaveButtonCom: FC = () => {
+        const { id } = useParams()
+        const { components = [] } = useGetComponentInfo()
+        const pageInfo = useGetPageSetting()
+
+        const { loading, run: save } = useRequest(
+            async () => {
+                if (!id) return
+                // 此处保存的实质为将对问卷进行修改后的信息调用以下请求方法同步到后端
+                await updateQuestionService(id, { ...pageInfo, components })
+            },
+            { manual: true }
+        )
+
+        // 快捷键
+        useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+            event.preventDefault()
+            if (!loading) save()
+        })
+
+        // 自动保存（不是定期保存，不是定时器）
+        useDebounceEffect(
+            () => {
+                save()
+            },
+            [components, pageInfo],
+            {
+                wait: 2000,
+            }
+        )
+
+        // TODO 手动保存debounce
+        return (
+            <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+                保存
+            </Button>
+        )
+    }
+
     return (
         <div className={styles['header-wrapper']}>
             <div className={styles.header}>
@@ -59,7 +103,7 @@ const EditHeader: FC = () => {
                 <div className={styles.main}><EditToolbar/></div>
                 <div className={styles.right}>
                     <Space>
-                        <Button>保存</Button>
+                        <SaveButtonCom/>
                         <Button type="primary">发布</Button>
                     </Space>
                 </div>
