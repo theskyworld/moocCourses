c3c1fd79c85d43349d306e7b76f93e98c8bb2a07
theskyworld <2456450687@qq.com>
2023-09-05 17:15:11 +0800

    * feat : add answer.ts & answer.js & fail.tsx & success.tsx

--- Sojump/Sojump/mock/router/index.js
+++ Sojump/Sojump/mock/router/index.js
@@ -1,11 +1,13 @@
 const user = require('./user.js');
 const question = require("./question.js");
 const stat = require('./stat.js');
+const answer = require("./answer.js")
 
 const mockList = [
     ...user,
     ...question,
     ...stat,
+    ...answer,
 ]
 
 module.exports = mockList;
