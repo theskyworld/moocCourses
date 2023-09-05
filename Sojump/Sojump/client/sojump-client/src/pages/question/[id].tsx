42ca285b4b963ceee20f456ab21d44f692278fb4
theskyworld <2456450687@qq.com>
2023-09-05 16:37:58 +0800

    * refactor : refactor question\[id].tsx

--- Sojump/Sojump/client/sojump-client/src/pages/question/[id].tsx
+++ Sojump/Sojump/client/sojump-client/src/pages/question/[id].tsx
@@ -1,36 +1,86 @@
 // 动态路由文件
 // 访问该文件时,输入的url中要包含id参数
 // 例如"/question/1"
-import Head from 'next/head'
+import PageWrapper from '@/components/PageWrapper'
+import { getQuestionById } from '@/services/question'
+import { getComponent } from '@/components/QuestionComponents'
+import styles from '@/styles/Question.module.scss'
 
-type PropsType = {
-    id: string
+interface QuestionProps{
+    errno: number,
+    data?: {
+        id: string
+        title: string
+        desc?: string
+        js?: string
+        css?: string
+        isPublished: boolean
+        isDeleted: boolean
+        componentList: Array<any>
+    }
+    msg?: string
 }
 
-export default function Question(props: PropsType) {
-    return <>
-        <Head>
-            <title>Next.js About</title>
-            <meta name="description" content="question page" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-            <h1>Question page</h1>
-            <p>{props.id}</p>
-        </main>
+export default function Question(props: QuestionProps) {
+    const { errno, data, msg = '' } = props
+
+    // 数据错误
+    if (errno !== 0) {
+        return <PageWrapper title="错误">
+            <h1>错误</h1>
+            <p>{msg}</p>
+        </PageWrapper>
+    }
+
+    const { id, title = '', desc = '', isDeleted, isPublished, componentList = [] } = data || {}
+
+    // 已经被删除的，提示错误
+    if (isDeleted) {
+        return <PageWrapper title={title} desc={desc}>
+            <h1>{title}</h1>
+            <p>该问卷已经被删除</p>
+        </PageWrapper>
+    }
+
+    // 尚未发布的，提示错误
+    if (!isPublished) {
+        return <PageWrapper title={title} desc={desc}>
+            <h1>{title}</h1>
+            <p>该问卷尚未发布</p>
+        </PageWrapper>
+    }
+
+    // 遍历组件
+    const ComponentListElem = <>
+        {componentList.map(c => {
+            const ComponentElem = getComponent(c)
+            return <div key={c.fe_id} className={styles.componentWrapper}>
+                {ComponentElem}
+            </div>
+        })}
     </>
-}
 
+    return <PageWrapper title={title} desc={desc}>
+        <form method='post' action="/api/answer">
+            <input type="hidden" name="questionId" value={id} />
+
+            {ComponentListElem}
+
+            <div className={styles.submitBtnContainer}>
+                {/* <input type="submit" value="提交"/> */}
+                <button type="submit">提交</button>
+            </div>
+        </form>
+    </PageWrapper>
+}
 
 export async function getServerSideProps(context: any) {
-    // 读取传入的id路由参数
-    const { id = "" } = context.params;
+    const { id = '' } = context.params
 
-    // 将获取的id值传入props中
+    // 根据 id 获取问卷数据
+    const data = await getQuestionById(id)
+
     return {
-        props: {
-            id,
-        }
+        props: data
     }
 }
