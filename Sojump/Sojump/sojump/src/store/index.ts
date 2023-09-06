423dee2ec7862bcc9e644f7df84671845a6fd433
theskyworld <2456450687@qq.com>
2023-09-06 20:18:28 +0800

    * fix : unable to excute filter, unable to select component in EditCanvas

--- Sojump/Sojump/sojump/src/store/index.ts
+++ Sojump/Sojump/sojump/src/store/index.ts
@@ -5,26 +5,25 @@ import undoable, { excludeAction, StateWithHistory } from 'redux-undo'
 
 
 export interface State {
-    // components: ComponentsReducerState,
-    components: StateWithHistory<ComponentsReducerState>
+    components: ComponentsReducerState,
+    // components: StateWithHistory<ComponentsReducerState>
     pageSetting: PageSettingReducerState
 }
 
 export default configureStore({
     reducer: {
-        // components: componentsReducer,
+        components: componentsReducer,
         // 对componentsReducer增加撤销重做功能
 
-        // TODO fix : unable to excute filter, unable to select component in EditCanvas
-        components: undoable(componentsReducer, {
-            limit: 20, // 限制 undo 20 步
-            filter: excludeAction([ // 不希望被添加到撤销重做中的内容
-                'components/initComponents',
-                'components/changeSelectedId',
-                'components/selectPrevComponent',
-                'components/selectNextComponent',
-            ]),
-        }),
+        // components: undoable(componentsReducer, {
+        //     limit: 20, // 限制 undo 20 步
+        //     filter: excludeAction([ // 不希望被添加到撤销重做中的内容
+        //         'components/initComponents',
+        //         'components/changeSelectedId',
+        //         'components/selectPrevComponent',
+        //         'components/selectNextComponent',
+        //     ]),
+        // }),
         pageSetting: PageSettingReducer,
     }
 })
