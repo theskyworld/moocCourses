30c11ae21975b5d97ae56499be2ecfd83988b7bf
theskyworld <2456450687@qq.com>
2023-09-09 11:12:41 +0800

    * fix : add fallback on route lazy load

--- Sojump/Sojump/sojump/src/App.tsx
+++ Sojump/Sojump/sojump/src/App.tsx
@@ -1,14 +1,27 @@
 // 列表页组件
-import React from "react";
+import React, { FC, Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import routerConfig from "./router/index";
 import "./assets/css/reset.scss";
 import "./assets/css/base.scss";
+import { Spin } from "antd";
 
 function App() {
+  const LoadingCom: FC = () => {
+    return (
+      <>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <Spin size="large" tip="Loading..."><span></span></Spin>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
-      <RouterProvider router={routerConfig}></RouterProvider>
+      <Suspense fallback={<LoadingCom/>}>
+        <RouterProvider router={routerConfig}></RouterProvider>
+      </Suspense>
     </>
   );
 }
